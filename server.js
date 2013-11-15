5cfcc7d6008caaa45ef14b09d6df4e353a5597bf
Felix Rieseberg <felix.rieseberg@microsoft.com>
2013-11-15 09:17:12 -0800

    Upload

--- server.js
+++ server.js
@@ -71,6 +71,5 @@ require('./routes/global')(app);
 **/
 
 everyauth.helpExpress(app);
-app.listen(process.env.PORT || 3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(process.env.PORT || 3001);
 
